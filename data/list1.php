86356797ce43c2e1e383f0d641f00bae2694e538
“rab1IT” <vf25g.messia@gmail.com>
2019-10-25 21:18:23 +0900

    test

--- data/list1.php
+++ data/list1.php
@@ -12,10 +12,13 @@ for($i = 1; $i <= 8; $i++){
 </head>
 <body>
     <ul>
+        <?php for($i = 1;$i <= 20;$i++){
+        //ここに連続する処理の内容を書き込む
         <li class="color-red">1</li>
         <li>2</li>
         <li class="color-red">3</li>
         <li>4</li>
+        } ?>
     </ul>
 </body>
 </html>
--- data/test1025.php
+++ data/test1025.php
@@ -1,17 +1,16 @@
 <?php
 
 for($i = 1; $i <= 100; $i++){
-    
     if($i % 3 === 0 ){
             echo 'Fizz<br>';
             }
-    if($i % 5 === 0 ){
+    elseif($i % 5 === 0 ){
             echo 'Buzz<br>';
             }
-    if($i % 5 === 0 && $i % 3 === 0 ){
+    elseif($i % 5 === 0 && $i % 3 === 0 ){
         echo 'FizzBuzz<br>';
         }  
     else{
-            echo '<br>';
+            echo $i .'<br>';
         } 
     }
