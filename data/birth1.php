7911e4c20647c6c248b0be26fdf9da060c548bc2
“rab1IT” <vf25g.messia@gmail.com>
2019-10-26 07:10:53 +0900

    test

---
+++ data/birth1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>生年月日を入力するフォームを作ろう</h1>
+<label for="year">西暦</label>
+<select name="year">
+<option value="1980">1980</option>
+<option value="1981">1981</option>
+</select>
+
+
+
+</body>
+</html>
