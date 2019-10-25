d034839f670889cb855dd87eaae881195c8026a1
“rab1IT” <vf25g.messia@gmail.com>
2019-10-25 21:11:26 +0900

    test

--- data/test1025.php
+++ data/test1025.php
@@ -1,10 +1,9 @@
 <?php
-$i = 1;
-while($i <= 100){
+
+for($i = 1; $i <= 100; $i++){
     if($i % 5 === 0 && $i % 3 === 0 ){
         echo 'FizzBuzz<br>';
         }
-        
     elseif($i % 3 === 0 ){
             echo 'Fizz<br>';
             }
