c23ebec8e23715bf7516424871ee34a6a2e8d91d
“rab1IT” <vf25g.messia@gmail.com>
2019-10-28 17:22:14 +0900

    test

--- data/list1.php
+++ data/list1.php
@@ -8,12 +8,13 @@
 <body>
     <ul>
         <?php for($i = 1 ; $i <= 20 ; $i++){
+            } ?>
         //ここに連続する処理の内容を書き込む
         <li class="color-red">1</li>
         <li>2</li>
         <li class="color-red">3</li>
         <li>4</li>
-        } ?>
+        
     </ul>
 </body>
 </html>
