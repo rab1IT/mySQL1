d580924f05270f8630ab5a3ba7cb8cf8527df630
“rab1IT” <vf25g.messia@gmail.com>
2019-10-29 15:32:17 +0900

    test

--- data/array_other.php
+++ data/array_other.php
@@ -3,6 +3,6 @@ $array1 = array();//配列として初期化
 $array2 = array();
 
 $array1 = array('リンゴ','みかん','バナナ');
-$array2 = array('name' >= '鈴木','hobby' => 'テニス','email' => 'samp@sanm.com');
+$array2 = array('name' => '鈴木','hobby' => 'テニス','email' => 'samp@sanm.com');
 
 echo $array['name'];
