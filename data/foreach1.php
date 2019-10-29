5170d1326382931d6bc8b92f76c090ee3d13fbbc
“rab1IT” <vf25g.messia@gmail.com>
2019-10-30 01:07:03 +0900

    test

--- data/foreach1.php
+++ data/foreach1.php
@@ -1,9 +1,9 @@
 <?php 
 $array = array(
-    'name' => '鈴木'
-    'hobby' => 'テニス'
-    'email' => 'sam@sam.com'
-);
-foreach($array as $key => $var){
-    echo $key.':'.$var.'<br>';
-}
+    'name' => '佐藤',
+    'age' => '27',
+    'blood' => 'B'
+    );
+   foreach($array as $key => $var){
+       echo $var.'<br>';
+   }
