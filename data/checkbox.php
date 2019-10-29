ddff87a64d11bb1a4f09afcdbf8b58a2c2ece008
“rab1IT” <vf25g.messia@gmail.com>
2019-10-30 08:13:40 +0900

    test

--- data/checkbox.php
+++ data/checkbox.php
@@ -2,15 +2,15 @@
 <body>
     <h1>チェックボックスを使ったフォーム</h1>
     <p>好きな色を選択してください（複数選択可）</p>
+
 <form action="checkbox_receive.php" method="POST">
 <p>
 <input type="checkbox" name="colors[]" value = "青">青
 <input type="checkbox" name="colors[]" value = "赤">赤
-<input type="checkbox" name="colors[]" value = "黄”>黄
-<input type="checkbox" name="colors[]" value = "青">青
-<input type="checkbox" name="colors[]" value = "赤">赤
+<input type="checkbox" name="colors[]" value = "白">白
+<input type="checkbox" name="colors[]" value = "グリーン">グリーン
 </p>
-<input type="submit>
+<input type="submit">
 </form>
 </body>
 </html>
--- data/checkbox_receive.php
+++ data/checkbox_receive.php
@@ -1,7 +1,5 @@
 <?php
-//チェックボックスの値を取得しましょう
 $colors = $_POST['colors'];
-var_dump($colors);
 ?>
 
 <html>
@@ -11,5 +9,11 @@ var_dump($colors);
 <body>
     <h1>受信ページ</h1>
     <h3>好きな色</h3>
+    <ul>
+    <?php foreach($colors as $var){ ?>
+    <li><?php echo htmlspecialchars($var,ENT_QUOTES ,'UTF-8');?></li>
+    <?php } ?>
+</ul>
+<p>あなたの好きな色は<?php echo implode('と',$colors);?>です。</p>
 </body>
 </html>
