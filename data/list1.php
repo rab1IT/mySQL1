035b4445e2dcd2414600e08f47059aee32f5da9e
“rab1IT” <vf25g.messia@gmail.com>
2019-10-29 11:04:18 +0900

    練習

--- data/list1.php
+++ data/list1.php
@@ -8,13 +8,12 @@
 <body>
     <ul>
         <?php for($i = 1 ; $i <= 20 ; $i++){
-            } ?>
-        //ここに連続する処理の内容を書き込む
-        <li class="color-red">1</li>
-        <li>2</li>
-        <li class="color-red">3</li>
-        <li>4</li>
-        
+            if($i % 2 === 1) {　?>
+            <li class="color-red">奇数</li>
+            <?php }else{ ?>
+            <li>偶数</li>
+            <?php }
+        } ?>
     </ul>
 </body>
 </html>
